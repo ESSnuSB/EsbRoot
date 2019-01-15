5e99a669029e0a5a46c12a14c1fdcc858a85f608
Budimir Klicek <budimir.klicek@irb.hr>
2019-01-15 16:24:20 +0100

    Moved Peter C.'s detector files to prototype/esb/ directory

---
+++ prototype/test/peter/evetest.C
@@ -0,0 +1,54 @@
+#include "FairLogger.h" 
+
+void evetest(void)
+{
+	fair::Logger::SetConsoleSeverity("DEBUG");	
+	auto infile = new TFile("evetest.root");
+	TTree *simtree; infile->GetObject("cbmsim", simtree);
+	
+	//~ int ntrack;
+	//~ simtree->SetBranchAddress("MCTrack", &ntrack);
+
+	TClonesArray *mctrack_arr = new TClonesArray("EsbMCTrack");
+	simtree->SetBranchAddress("MCTrack", &mctrack_arr);
+	TClonesArray *detpoint_arr = new TClonesArray("EsbWCdetectorPoint");
+	simtree->SetBranchAddress("EsbWCdetectorPoint", &detpoint_arr);
+
+	simtree->GetEntry(0);
+	cout << mctrack_arr->GetEntries() << endl;
+
+	for(int a = 0; a < mctrack_arr->GetEntries(); ++a) {
+		EsbMCTrack *mctrack = (EsbMCTrack*) mctrack_arr->At(a);
+		cout << a << " " 
+			<< mctrack->GetPdgCode() << " "
+			<< mctrack->GetMotherId() << " "
+			<< "(" << mctrack->GetPx() << " " << mctrack->GetPy() << " " << mctrack->GetPz() << ") "
+			<< "(" << mctrack->GetStartX() << " " << mctrack->GetStartY() << " " << mctrack->GetStartZ() << ")"
+			<< endl;
+		//~ mctrack->Print(a);
+	}
+
+	for(int a = 0; a < detpoint_arr->GetEntries(); ++a) {
+		EsbWCdetectorPoint *detpoint = (EsbWCdetectorPoint*) detpoint_arr->At(a);
+		//~ cout << a << " " 
+			//~ << mctrack->GetPdgCode() << " "
+			//~ << mctrack->GetMotherId() << " "
+			//~ << "(" << mctrack->GetPx() << " " << mctrack->GetPy() << " " << mctrack->GetPz() << ") "
+			//~ << "(" << mctrack->GetStartX() << " " << mctrack->GetStartY() << " " << mctrack->GetStartZ() << ")"
+			//~ << endl;
+		detpoint->Print("");
+	}
+
+	
+	//~ TestTrack testtrack;
+	
+}
+
+   //~ Int_t    GetPdgCode()  const { return fPdgCode; }
+    //~ Int_t    GetMotherId() const { return fMotherId; }
+    //~ Double_t GetPx()       const { return fPx; }
+    //~ Double_t GetPy()       const { return fPy; }
+    //~ Double_t GetPz()       const { return fPz; }
+    //~ Double_t GetStartX()   const { return fStartX; }
+    //~ Double_t GetStartY()   const { return fStartY; }
+    //~ Double_t GetStartZ()   const { return fStartZ; }
