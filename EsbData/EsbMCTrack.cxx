c52a0cc0615c85821454de2ac8836783643be343
bklicek <42571178+bklicek@users.noreply.github.com>
2019-01-31 11:48:16 +0100

    Commented out the "debug" symbol which seems to sometime break compilation.

--- EsbData/EsbMCTrack.cxx
+++ EsbData/EsbMCTrack.cxx
@@ -111,9 +111,9 @@ EsbMCTrack::~EsbMCTrack() { }
 void EsbMCTrack::Print(Int_t trackId) const
 {
   //~ LOG(debug) << "Track " << trackId << ", mother : " << fMotherId << ", Type "
-  LOG(debug) << "Track " << trackId << ", mother : " << fMotherId << ", Type "
-             << fPdgCode << ", momentum (" << fPx << ", " << fPy << ", "
-             << fPz << ") GeV";
+  //LOG(debug) << "Track " << trackId << ", mother : " << fMotherId << ", Type "
+  //           << fPdgCode << ", momentum (" << fPx << ", " << fPy << ", "
+  //           << fPz << ") GeV";
  /* LOG(debug2) << "       Ref " << GetNPoints(kREF)
               << ", TutDet " << GetNPoints(kTutDet)
               << ", Rutherford " << GetNPoints(kFairRutherford);
