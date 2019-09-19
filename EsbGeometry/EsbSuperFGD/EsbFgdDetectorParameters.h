06f014c9446e5b2609363732d83a4497511e3ddc
rigel0586 <rigel0586@gmail.com>
2019-09-19 18:52:31 +0300

    Add parameter for graph limiting neightbour size

--- EsbGeometry/EsbSuperFGD/EsbFgdDetectorParameters.h
+++ EsbGeometry/EsbSuperFGD/EsbFgdDetectorParameters.h
@@ -48,6 +48,7 @@ namespace DP
     static const string FGD_TIME_INTERVAL_HITS = "timeIntervalHits";
     static const string FGD_EXCLUDE_HITS_ABOVE_TIME_INTERVAL = "excludeHitsAbove";
     static const string FGD_EXCLUDE_HITS_BELOW_TIME_INTERVAL = "excludeHitsBelow";
+    static const string FGD_MAX_NEIGHTBOUR_NODES = "maxNeightbourNodes";
 
     static const string PF_USE_VERTEX = "use_vertex";
     static const string PF_VERTEXX = "vertexX";
