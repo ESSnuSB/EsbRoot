236ad59c43c09bc4873a90d423a6022474d8b816
rigel0586 <rigel0586@gmail.com>
2019-11-14 21:07:37 +0200

    Adding initial data extraction from event generator

--- EsbReconstruction/EsbSuperFGD/FgdMCEventRecord.cxx
+++ EsbReconstruction/EsbSuperFGD/FgdMCEventRecord.cxx
@@ -1,7 +1,15 @@
 #include "EsbReconstruction/EsbSuperFGD/FgdMCEventRecord.h"
 
 
+#include "FairLogger.h"
 
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <math.h>
+#include <bits/stdc++.h>
 
 namespace esbroot {
 namespace reconstruction {
@@ -9,16 +17,22 @@ namespace superfgd {
 
 
 // -----   Constructors and Destructors  -----------------------------------
-FgdMCEventRecord::FgdMCEventRecord()
+
+FgdMCEventRecord::FgdMCEventRecord(std::string eventData) 
+    : feventData(eventData), fIsInit(false)
+{
+    Init();
+}
+
+FgdMCEventRecord::FgdMCEventRecord() 
+    : feventData(""), fIsInit(false)
 {
 }
 
 FgdMCEventRecord::FgdMCEventRecord(const FgdMCEventRecord& c)
 {
-    this->fNu_pdg = c.fNu_pdg;
-    this->fNu_E = c.fNu_E;
-    this->fIsWeakCC = c.fIsWeakCC;
-    this->fIsWeakNC = c.fIsWeakNC;
+    this->feventData = c.feventData;
+    this->Init();
 }
 
 FgdMCEventRecord::~FgdMCEventRecord()
@@ -27,18 +41,118 @@ FgdMCEventRecord::~FgdMCEventRecord()
 // -------------------------------------------------------------------------
 
 // -----   Public methods   ------------------------------------------------
+
+void FgdMCEventRecord::SetEventData(std::string data)
+{
+    fIsInit = false;
+    feventData = data;
+    Init();
+}
+
+Int_t FgdMCEventRecord::GetNuPdg(void)
+{
+    return std::stoi(fDataTokens[Data::NEUTRINO_PDG]);
+}
+
+Double_t FgdMCEventRecord::GetNuE(void)
+{
+    return std::stod(fDataTokens[Data::NEUTRINO_ENERGY]);
+}
+
+TVector3 FgdMCEventRecord::GetVertex(void)
+{
+    Double_t x = std::stod(fDataTokens[Data::VERTEX_POSTION_X]);
+    Double_t y = std::stod(fDataTokens[Data::VERTEX_POSTION_Y]);
+    Double_t z = std::stod(fDataTokens[Data::VERTEX_POSTION_Z]);
+
+    return TVector3(x,y,z);
+}
+
+Bool_t FgdMCEventRecord::IsWeakCC(void)
+{
+    Int_t val = std::stoi(fDataTokens[Data::IS_WEAK_CC]);
+    return (val==1);
+}
+
+Bool_t FgdMCEventRecord::IsWeakNC(void)
+{
+    Int_t val = std::stoi(fDataTokens[Data::IS_WEACK_NC]);
+    return (val==1);
+}
+
+Bool_t FgdMCEventRecord::IsQuasiElastic(void)
+{
+    Int_t val = std::stoi(fDataTokens[Data::IS_QUASI_ELASTIC]);
+    return (val==1);
+}
+
 FgdMCEventRecord& FgdMCEventRecord::operator=(const FgdMCEventRecord& c)
 {
-    this->fNu_pdg = c.fNu_pdg;
-    this->fNu_E = c.fNu_E;
-    this->fIsWeakCC = c.fIsWeakCC;
-    this->fIsWeakNC = c.fIsWeakNC;
+    this->feventData = c.feventData;
+    this->Init();
     return *this;
 }
+
+std::vector<std::pair<Int_t, TVector3>> FgdMCEventRecord::GetPrimaryParticles()
+{
+    size_t particleDataSize = 4;
+
+    size_t particlePdg = 0;
+    size_t particleMomX = 1;
+    size_t particleMomY = 2;
+    size_t particleMomZ = 3;
+
+    std::vector<std::pair<Int_t, TVector3>> particles;
+    for(size_t i = Data::PRIMARY_PARTICLES; i < fDataTokens.size(); i+=particleDataSize)
+    {
+        Int_t pdg = std::stoi(fDataTokens[i + particlePdg]);
+        Double_t momX = std::stod(fDataTokens[i + particleMomX]);
+        Double_t momY = std::stod(fDataTokens[i + particleMomY]);
+        Double_t momZ = std::stod(fDataTokens[i + particleMomZ]);
+
+        particles.push_back(
+                            std::pair<Int_t, TVector3>( pdg,    TVector3(momX , momY, momZ) )
+                            );
+    }
+    return particles;
+}
+
+void FgdMCEventRecord::PrintData(std::ostream & stream)
+{
+    stream << feventData;
+}
+
 // -------------------------------------------------------------------------
 
 
-// -----   Private methods   -----------------------------------------------
+// -----   Protected methods   ---------------------------------------------
+void FgdMCEventRecord::Init()
+{
+    if(fIsInit) return;
+
+    fIsInit = true;
+    fDataTokens.clear();
+
+    try
+    {        
+        const char spaceChar(' ');
+
+        std::istringstream ss(feventData);
+        std::string token;
+        while(std::getline(ss, token, spaceChar))
+        {
+            if(!token.empty())
+            {
+                fDataTokens.push_back(token);
+            }
+        }
+    }
+    catch(const std::exception& e)
+    {
+        LOG(fatal) << e.what();
+        fIsInit = false;
+    }
+}
 
 // -------------------------------------------------------------------------
 
--- EsbReconstruction/EsbSuperFGD/FgdMCEventRecord.h
+++ EsbReconstruction/EsbSuperFGD/FgdMCEventRecord.h
@@ -2,7 +2,10 @@
 #define ESBROOT_ESBDRECONSTRUCTION_FGD_MC_EVENT_RECORDT_H
 
 #include <TObject.h>
+#include <TVector3.h>
 
+#include <vector>
+#include <ostream>
 
 namespace esbroot {
 namespace reconstruction {
@@ -11,6 +14,9 @@ namespace superfgd {
 class FgdMCEventRecord : public TObject
 {
 public:
+
+    FgdMCEventRecord(std::string eventData);
+
     FgdMCEventRecord();
 
     /* Copy constructor */
@@ -21,22 +27,42 @@ public:
     /* Operators */
     FgdMCEventRecord& operator=(const FgdMCEventRecord& c);
 
-    void SetNuPdg(Int_t pdg){fNu_pdg = pdg;}
-    void SetNuE(Double_t e){fNu_E = e;}
-    void SetWeakCC(Int_t cc){fIsWeakCC = cc;}
-    void SetWeakNC(Int_t nc){fIsWeakNC = nc;}
+    void SetEventData(std::string data);
 
-    Int_t GetNuPdg(void){return fNu_pdg;}
-    Double_t GetNuE(void){return fNu_E;}
-    Int_t GetWeakCC(void){return fIsWeakCC;}
-    Int_t GetWeakNC(void){return fIsWeakNC;}
+    Int_t GetNuPdg(void);
+    Double_t GetNuE(void);
+    TVector3 GetVertex(void);
+    Bool_t IsWeakCC(void);
+    Bool_t IsWeakNC(void);
+    Bool_t IsQuasiElastic(void);
+    std::vector<std::pair<Int_t, TVector3>> GetPrimaryParticles();
+
+    std::string GetEventData(){return feventData; }
+
+    void PrintData(std::ostream & stream);
 
 protected:
 
-    Int_t fNu_pdg;
-    Double_t fNu_E;
-    Int_t fIsWeakCC;
-    Int_t fIsWeakNC;
+    enum Data : int   // Position in the fDataTokens which value represents which data 
+    {
+        NEUTRINO_PDG = 0,
+        NEUTRINO_ENERGY = 1,
+        IS_WEAK_CC = 2,
+        IS_WEACK_NC = 3,
+        IS_QUASI_ELASTIC = 4,
+        VERTEX_POSTION_X = 5,
+        VERTEX_POSTION_Y = 6,
+        VERTEX_POSTION_Z = 7,
+        PRIMARY_PARTICLES = 8   // The primary particles are written as 4 values - 1st pdg then momentum X, momentum Y, momentum Z
+                                // all data above 8th position are primary particle data
+    };
+
+    void Init();
+    Bool_t fIsInit;
+
+    std::string feventData;
+
+    std::vector<std::string> fDataTokens;//!<!
 
     ClassDef(FgdMCEventRecord, 2);
 
